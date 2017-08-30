e563680c1883c0a80cfb59802a641e755ef10078
guolin.huang <guolin.huang@advantech.com.cn>
2017-08-30 12:29:58 +0800

    update 2.6

--- ch2/2.6.rkt
+++ ch2/2.6.rkt
@@ -19,3 +19,15 @@
   (lambda (f) 
     (lambda (x) 
       ((a f) ((b f) x)))))
+; test
+(define (square x) (* x x))
+((one square) 2) ; 4
+((two square) 2) ; 16
+(((add two one) square) 2) ; 256
+; 通过以上测试代码，可以看出，这里定义数字多少
+; 的方式,是把它转化为操作，2就是两次操作, 
+; n 就是 n 次操作。
+; 对应到zero/one/two/add等定义中返回的函数，
+; 它的参数是 f, 代表操作;
+; 它的返回值是另外一个函数，这个函数的参数是 x,
+; 代表操作对象，对应到测试代码，就是数字。
