13a2201911a1a72deca3cd992d325c6b5d257515
guolin.huang <guolin.huang@advantech.com.cn>
2017-09-05 17:50:47 +0800

    finished 2.48

---
+++ ch2/2.48.rkt
@@ -0,0 +1,23 @@
+#lang scheme
+;from 2.46
+(define (make-vect x y) (cons x y))
+(define (xcor-vect v) (car v))
+(define (ycor-vect v) (cdr v))
+(define (add-vect v w)
+  (make-vect (+ (xcor-vect v)
+                (xcor-vect w))
+             (+ (ycor-vect v)
+                (ycor-vect w))))
+(define (sub-vect v w)
+  (make-vect (- (xcor-vect v)
+                (xcor-vect w))
+             (- (ycor-vect v)
+                (ycor-vect w))))
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v))
+             (* s (ycor-vect v))))
+
+; define segment constructor and selectors
+(define make-segment cons) 
+(define start-segment car) 
+(define end-segment cdr) 
