625fe397d2708ea62a16b0136d030931a9121c52
Alvin Huang <huanguolin@outlook.com>
2017-11-06 22:03:58 +0800

    finished 3.81

--- ch3/3.5.5/3.81.rkt
+++ ch3/3.5.5/3.81.rkt
@@ -2,13 +2,40 @@
 
 (require (file "../3.5.1/stream.rkt"))
 
-(define rand
-  (define rand-nums
-    (cons-stream random-init
-                 (stream-map rand-update rand-sums)))
-  (define (dispatch message) 
-    (cond ((eq? message 'generate) 
-           (stream) 
-           ((eq? message 'reset) 
-            (lambda (new-value) (set! x new-value))))) 
-    dispatch) 
+; 参考: https://github.com/jiacai2050/sicp/blob/master/exercises/03/3.81.scm
+
+(define (rand-update x)
+  (let ((a (expt 2 32))
+        (c 1103515245)
+        (m 12345))
+    (modulo (+ (* a x) c) m)))
+
+(define random-init 137)
+
+(define (random-numbers cmd-stream)
+  (define (action number cmd)
+    (if (eq? cmd 'generate)
+        (rand-update number)
+        cmd))
+  (cons-stream random-init
+               (stream-map action
+                           (random-numbers cmd-stream)
+                           cmd-stream)))
+
+; test
+(define cmds-1
+  (cons-stream 'generate 
+               (cons-stream 'generate
+                            (cons-stream 'generate
+                                         (cons-stream 'generate '())))))
+(define s1 (random-numbers cmds-1))
+(display-stream-top s1 4)
+
+; test
+(define cmds-2
+  (cons-stream 'generate 
+               (cons-stream 137
+                            (cons-stream 'generate
+                                         (cons-stream 'generate '())))))
+(define s2 (random-numbers cmds-2))
+(display-stream-top s2 4)
