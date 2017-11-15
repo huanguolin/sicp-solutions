299a4bd1e505dd934c96a2aa2bd0dea7a0765697
alvin huang <huanguolin@outlook.com>
2017-11-15 17:47:10 +0800

    update

--- generate-exercises-list/generate_exercises_list.rkt
+++ generate-exercises-list/generate_exercises_list.rkt
@@ -1,20 +1,20 @@
 #lang racket
 
-(define exercises-path "./exercises/ch1/1.3.1")
+(define (list-exercises path)
+  (define (path-proc x)
+    (string-join
+     (drop-right
+      (string-split
+       (path->string x) ".") 1) "."))
+  (map path-proc
+       (directory-list path)))
+; test
+(list-exercises (string->path "../exercises/ch1/1.3.1"))
 
-(path-string? exercises-path)
-(directory-exists? (string->path exercises-path))
-(directory-exists? (string->path "./abc"))
+(define (list-dirs path)
+  (filter directory-exists?
+          (directory-list path
+                          #:build? #t)))
+; test
+(list-dirs (string->path "../exercises/ch1/"))
 
-(define (path-proc x)
-  (string-join
-    (drop-right
-     (string-split
-      (path->string x) ".") 1) "."))
-
-(map path-proc
- (directory-list
-  (string->path exercises-path)))
-
-;(find-files file-exists?
-;            (string->path exercises-path))
