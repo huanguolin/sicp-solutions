bd3a7feab7e3aff99a9b84a3551194cb715c9a37
Alvin Huang <huanguolin@outlook.com>
2019-03-17 15:42:26 +0800

    format

--- exercises/ch3/3.1.1/3.3.rkt
+++ exercises/ch3/3.1.1/3.3.rkt
@@ -3,18 +3,17 @@
 (define (make-account balance password)
   (define (withdraw amount)
     (if (>= balance amount) 
-	(begin (set! balance (- balance amount))
-	       balance)
-	"Insufficient funds"))
+        (begin (set! balance (- balance amount)) balance)
+	      "Insufficient funds"))
   (define (deposit amount)
-    (set! balance (+ balance amount))
+    (set! balance (+ balance amount)) 
     balance)
   (define (dispatch pw m)
     (cond ((not (eq? pw password)) (error "Incorrect password"))
       	  ((eq? m 'withdraw) withdraw)
-	  ((eq? m 'deposit) deposit)
-	  (else (error "Unknown request: MAKE-ACCOUNT"
-		       m))))
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request: MAKE-ACCOUNT"
+                m))))
   dispatch)
 
 ; test
